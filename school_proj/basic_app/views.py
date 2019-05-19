d60925d3694cce592efd9fbb727436b96697f38c
Your Name <your@email.com>
2019-05-19 16:18:21 +0530

    'second'

---
+++ school_proj/basic_app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from basic_app import models
+from django.urls import reverse_lazy
+# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['inject_me'] = 'Injected Data'
+        return context
+
+class SchoolListView(ListView):
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_details'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    context_object_name = 'school'
+    fields = ('name','principal','location')
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields=("name","principal")
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
